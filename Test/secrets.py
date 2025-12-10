18f0bb067e1b3bfe53c165a857f15dd31bcf547a
Prestige <filipmoldrup@gmail.com>
2025-12-10 10:25:23 +0100

    Update SERVER_IP_ADDRESS format in secrets.py
    Changed SERVER_IP_ADDRESS from a full API URL to just the IP address. This simplifies the variable and may be intended for use in a different context where only the IP is needed.

--- Test/secrets.py
+++ Test/secrets.py
@@ -4,4 +4,4 @@ PASSWORD = 'TeamAGP3'
 
 # Server credentials
 ACCESS_TOKEN = 'OKcAjXshr9Ha82R4sJUM'
-SERVER_IP_ADDRESS = 'http://10.20.0.4:8080/api/v1/OKcAjXshr9Ha82R4sJUM/telemetry'
+SERVER_IP_ADDRESS = '10.20.0.4'
